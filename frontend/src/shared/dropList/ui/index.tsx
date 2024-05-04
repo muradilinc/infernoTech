9b6d9b46a269b86aeec56886651e56e69a7aca4a
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-05-05 00:48:47 +0600

    clear import

--- frontend/src/shared/dropList/ui/index.tsx
+++ frontend/src/shared/dropList/ui/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../app/store/hooks';
 import { selectCategories } from '../../../features/categories/categoriesSlice';
 import { getAllCategory } from '../../../features/categories/categoriesThunk';
